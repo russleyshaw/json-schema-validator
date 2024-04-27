0bf7ef360541b6dbe67bc8ee3f8a75a63f20afbb
Russley Shaw <russleyshaw@gmail.com>
2024-04-27 15:13:48 -0500

    Use hash router

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,8 @@
-import { RouterProvider, createRouter } from "@tanstack/react-router";
+import {
+	RouterProvider,
+	createHashHistory,
+	createRouter,
+} from "@tanstack/react-router";
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 
@@ -10,7 +14,8 @@ import { routeTree } from "./routeTree.gen";
 import "./index.css";
 
 // Create a new router instance
-const router = createRouter({ routeTree });
+const hashHistory = createHashHistory();
+const router = createRouter({ routeTree, history: hashHistory });
 
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
